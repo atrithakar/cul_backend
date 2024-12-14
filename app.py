ba9dbaf5e46a4dd150b465159eec61ca887731f6
Atri Thakar <atrithaker122@gmail.com>
2024-12-14 21:14:56 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, send_from_directory, abort, send_file
 import os
 from pathlib import Path
+import zipfile
 
 app = Flask(__name__)
 
-BASE_DIR = "c_cpp_modules"  # Directory to serve files from
+BASE_DIR = "c_cpp_modules"  # Directory containing all modules and versions
 
-@app.route('/files', methods=['GET'])
-def serve_files():
+@app.route('/files/<module_name>/<version>', methods=['GET'])
+def serve_files(module_name, version):
+    module_dir = os.path.join(BASE_DIR, module_name, version)
+
     try:
-        # Send a ZIP file containing all the files from BASE_DIR
-        zip_filename = 'all_files.zip'
-        zip_path = Path(BASE_DIR) / zip_filename
+        # Send a ZIP file containing all the files from the specified module version directory
+        zip_filename = f'{module_name}_{version}.zip'
+        zip_path = Path(module_dir) / zip_filename
 
-        # Create a zip file containing all files in the BASE_DIR
-        with zipfile.ZipFile(zip_path, 'w') as zipf:
-            for root, dirs, files in os.walk(BASE_DIR):
-                for file in files:
-                    zipf.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), BASE_DIR))
+        # Create a zip file containing all files in the module version directory
+        # with zipfile.ZipFile(zip_path, 'w') as zipf:
+        #     for root, dirs, files in os.walk(module_dir):
+        #         for file in files:
+        #             zipf.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), module_dir))
         
         return send_file(zip_path, as_attachment=True)
     except Exception as e:
