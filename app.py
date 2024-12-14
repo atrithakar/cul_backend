e81b46feeaf3c8f5b7c5221eaa408ac5a44b4612
Atri Thakar <atrithaker122@gmail.com>
2024-12-14 21:22:27 +0530

    minor changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory, abort, send_file
 import os
 from pathlib import Path
 import zipfile
+import io
 
 app = Flask(__name__)
 
@@ -12,17 +13,16 @@ def serve_files(module_name, version):
     module_dir = os.path.join(BASE_DIR, module_name, version)
 
     try:
-        # Send a ZIP file containing all the files from the specified module version directory
-        zip_filename = f'{module_name}_{version}.zip'
-        zip_path = Path(module_dir) / zip_filename
-
-        # Create a zip file containing all files in the module version directory
-        with zipfile.ZipFile(zip_path, 'w') as zipf:
+        # Create an in-memory zip file
+        zip_stream = io.BytesIO()
+        with zipfile.ZipFile(zip_stream, 'w') as zipf:
             for root, dirs, files in os.walk(module_dir):
                 for file in files:
-                    zipf.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), module_dir))
-        
-        return send_file(zip_path, as_attachment=True)
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, os.path.relpath(file_path, module_dir))
+
+        zip_stream.seek(0)  # Go back to the start of the stream
+        return send_file(zip_stream, as_attachment=True, download_name=f"{module_name}_{version}.zip")
     except Exception as e:
         print(f"Error: {e}")
         return "Error occurred while serving files", 500
