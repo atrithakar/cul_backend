c4d1ad293eb58fc070319e11bada122e92d23179
Atri Thakar <atrithaker122@gmail.com>
2024-12-18 00:41:05 +0530

    Update corresponding to new feature in cul

--- app.py
+++ app.py
@@ -77,5 +77,26 @@ def serve_files_2(module_name):
         return jsonify({"error": "An error occurred."}), 500
 
 
+@app.route('/versions/<module_name>', methods=['GET'])
+def get_versions(module_name):
+    versions_file_path = os.path.join(BASE_DIR, module_name, 'versions.json')
+    
+    # Check if the module directory exists
+    if not os.path.exists(os.path.join(BASE_DIR, module_name)):
+        return jsonify({"error": f"Module '{module_name}' not found."}), 404
+
+    # Check if the versions.json file exists
+    if not os.path.exists(versions_file_path):
+        return jsonify({"error": "The versions.json file is missing for the specified module."}), 404
+
+    try:
+        with open(versions_file_path, 'r') as file:
+            data = json.load(file)
+            return jsonify(data)
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding the versions.json file."}), 500
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "An error occurred."}), 500
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
